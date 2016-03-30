4bc9aa5d116ed31e569ed36fc9d0eb76bed321d9
Achromic <music1384@yahoo.co.uk>
2016-03-30 11:44:18 +0100

    Created a mouse click/touch input event listener to start the game from the menu. Also added simple debug text to visually inform the user when the game is in the menu state. This will eventually be replaced by a full splash screen.

--- Breakout Phaser/js/States/menu.js
+++ Breakout Phaser/js/States/menu.js
@@ -9,6 +9,12 @@ MenuState.prototype =
     create: function ()
     {
         console.log("Menu State Loaded");
+        
+        // Create a listener event for a mouse click or touch and call's the function when it occurs.
+        game.input.onDown.add(this.startGame);
+        
+        // Simple debug menu label.
+		var menuLabel = game.add.text(400, 300, 'Click to continue', {font: '30px Courier', fill: '#ffffff'});
     },
     
     // Start's the game state so that gameplay begins.
