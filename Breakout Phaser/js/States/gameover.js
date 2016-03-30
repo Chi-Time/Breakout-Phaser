93e0465872eb31bfbd4abd54fec878c9f65d720c
Achromic <music1384@yahoo.co.uk>
2016-03-31 00:02:33 +0100

    Created event listener to transition back from game over state to the main game state.

--- Breakout Phaser/js/States/gameover.js
+++ Breakout Phaser/js/States/gameover.js
@@ -2,13 +2,16 @@
 var GameOverState = function (game)
 {};
 
-
+// Extending the game over state.
 GameOverState.prototype =
 {
     // Create's and initialises all required assets for the game over state.
     create: function ()
     {
         console.log("Game Over State Loaded");
+        game.world.removeAll();
+        // Create a listener event for a mouse click or touch and call's the function when it occurs.
+        game.input.onDown.add(this.restart);
     },
     
     // Restarts the game state so that gameplay begins fresh and from the beginning.
