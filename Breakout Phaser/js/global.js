0bbd9011bb2ae997910d91994badb00e38ebeddb
Achromic <music1384@yahoo.co.uk>
2016-03-30 12:03:24 +0100

    Created global .js file. This file contains all of the (so far known) game's variables so that they can be accessed across the scripts. These will eventually be filtered down into each state, but for development purposes this is functional.

---
+++ Breakout Phaser/js/global.js
@@ -0,0 +1,22 @@
+// The ball object.
+var ball = {};
+// The paddle object.
+var paddle = {};
+// The brick group.
+var bricksGroup = {};
+// The score text object.
+var scoreText = {};
+// The lives text object.
+var livesText = {};
+// The intro text object.
+var introText = {};
+// The starfield background image.
+var startFieldBackground = {};
+
+// Boolean to tell if ball is on the paddle.
+var ballOnPaddle = true;
+
+// The number of the lives the player has.
+var lives = 3;
+// The current score of the game.
+var score = 0;
