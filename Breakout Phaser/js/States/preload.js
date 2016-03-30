eaa04988471c17c42ca0007427ef0b037938b675
Achromic <music1384@yahoo.co.uk>
2016-03-30 11:41:43 +0100

    Added file path's and key's for the prototype game assets to load. Also created a simple debug text to inform when the game is loading. This will eventually be replaced with a cropped loading bar.

--- Breakout Phaser/js/States/preload.js
+++ Breakout Phaser/js/States/preload.js
@@ -9,6 +9,15 @@ PreloadState.prototype =
     preload: function ()
     {
         console.log("Preload State Loaded");
+        
+        // Load the required game images.
+        game.load.atlas('breakout', 'assets/sprites/breakout.png', 'assets/sprites/breakout.json');
+        game.load.image('starfield', 'assets/sprites/starfield.png');
+        
+        // Simple debug loading label.
+		var loadingLabel = game.add.text(80, 150, 'loading...', {font: '30px Courier', fill: '#ffffff'});
+        
+        //TODO: Create and add loadingBar sprite.
     },
     
     // Create's and initialises all required assets for the preload state. (Called directly after preload has finished.)
